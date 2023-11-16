31c3c332c62509c37b14d35b46cfb4968466205d
Tiago Eloy Possidonio <105402460+CheweeBR@users.noreply.github.com>
2023-11-16 14:00:32 -0300

    Update javascript.js

--- src/javascript.js
+++ src/javascript.js
@@ -8,8 +8,8 @@ chamados.addEventListener("keypress", function(e) {
 
 function Search(chamado) {
     let vetor = chamado.match(/.{1,7}/g);
-    if(vetor.length > 25) {
-        alert("Não é possível abrir mais de 25 chamados por vez!");
+    if(vetor.length > 50) {
+        alert("Não é possível abrir mais de 50 chamados por vez!");
     } else {
     for(let c=0; c<vetor.length;c++) {
             window.open("https://top.br.atos.net/canais/sc/chamados/detail_request.cfm?id="+vetor[c], "_blank");
